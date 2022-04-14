79930f9c624b89da2f06381eefb9aec62fe8f206
Milene <milene.jannetti@gmail.com>
2022-04-14 16:03:30 -0300

    Enviar mensagem OK

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let nomeObj = {}
+let mensagem = {
+    from:"",
+    to:"",
+    text:"",
+    type:""
+}
 function entrarNaSala() {
     const nome = document.querySelector(".tela-entrada input").value;
     nomeObj = { name: nome };
     let promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomeObj);
     promise.then(aparecerChat);
     promise.catch(tratarErro);
+    document.querySelector(".tela-entrada input").value="";
 }
 
 function aparecerChat() {
@@ -17,6 +24,9 @@ function tratarErro(erro) {
     if (codigo === 400) {
         alert("Já existe um usuário online com esse nome.\nPor favor escolha outro nome.");
     }
+}
+function atualizarPagina(){
+    window.location.reload();
 }
 function manterStatus() {
     axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nomeObj);
@@ -43,19 +53,47 @@ function renderizarMsgs(response) {
 </div>`
     }
     let MsgNova = document.querySelector(".container > div:last-child > p");
-    if(MsgNova.innerHTML!==MsgPrevia.innerHTML){
-        alert("Mensagem nova!!");
+    if(MsgPrevia===null){
+        MsgNova.scrollIntoView();
+    }else if(MsgNova.innerHTML!==MsgPrevia.innerHTML){
         MsgNova.scrollIntoView();
     }
 }
 function enviarMensagem() {
     const conteudo = document.querySelector(".rodape input").value;
-    let mensagem = {
-        from: nomeObj.name,
-        to: "Todos",
-        text: conteudo,
-        type: "message"
-    }
-    let promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', mensagem);
+mensagem.from=nomeObj.name;
+if(mensagem.to===""){mensagem.to="Todos"}
+mensagem.text=conteudo;
+if(mensagem.type===""){mensagem.type="message"}
+let promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', mensagem);
     promise.then(refrescarChat);
+    promise.catch(atualizarPagina);
+    document.querySelector(".rodape input").value="";
 }
+function aparecerMenu(){
+    document.querySelector(".caixa-menu").classList.remove("escondido");
+}
+function desaparecerMenu(){
+    document.querySelector(".caixa-menu").classList.add("escondido");
+}
+
+function selecionarContato(element){
+    let listaContatos = document.querySelectorAll(".contatos .check");
+    for(let i=0;i<listaContatos.length;i++){
+    listaContatos[i].classList.add("escondido");
+    }
+    element.querySelector(".check").classList.remove("escondido");
+    let novoContato = element.querySelector("p").innerHTML;
+    mensagem.to=novoContato
+}
+
+function selecionarVisib(element){
+    let listaVisib = document.querySelectorAll(".visibilidade .check");
+    for(let i=0;i<listaVisib.length;i++){
+        listaVisib[i].classList.add("escondido");
+        }
+        element.querySelector(".check").classList.remove("escondido");
+        let novaVisib = element.querySelector("p").innerHTML;
+        if(novaVisib==="Reservadamente"){mensagem.type="private_message"}
+        if(novaVisib==="Público"){mensagem.type="message"}
+    }
